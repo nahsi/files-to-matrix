222079945794b1b02009f5470a083f06d636115e
Anatoly Laskaris <nahsi@airmail.cc>
2022-01-02 12:30:44 +0300

    Fixes

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import * as core from 'https://jspm.dev/@actions/core@1.6.0';
 const testInput = core.getInput('testInput');
 
 async function run() {
-  core.warning('Hello')
-  core.debug('${testInput}')
+  core.warning(`Hello`);
+  core.debug(`${testInput}`);
 }
 
 run();
